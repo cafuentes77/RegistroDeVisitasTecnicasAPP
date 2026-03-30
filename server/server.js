08d73165467140ade50cbe0827739150978f1b75
cafuentes <cfuentesmedina@gmail.com>
2026-03-29 21:22:57 -0300

    feat:se genera creacion de nuevo administrador, sin eliminar el administrador anterior

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(
   cors({
     origin: "http://localhost:5173", // Ajusta según tu frontend
     credentials: true,
-  })
+  }),
 );
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
