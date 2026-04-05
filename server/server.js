a07a91791264db7eccec474c6c16bb7a3fe839b7
cafuentes <cfuentesmedina@gmail.com>
2026-04-04 22:31:14 -0300

    Actualizar URLs de API y CORS para producción

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ import { fileURLToPath } from "url";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // Conexión a MongoDB
 mongoose
